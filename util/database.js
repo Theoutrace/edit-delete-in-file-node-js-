660edef890e26a6c7ed24cd3ccd99cef792fb751
Theoutrace <prakashkumar.imw@gmail.com>
2023-01-20 23:45:04 +0530

    Integrated sequalize and performed CRUD operation.

--- util/database.js
+++ util/database.js
@@ -1,11 +1,8 @@
-const mysql = require("mysql2");
+const Sequelize = require("sequelize");
 
-const pool = mysql.createPool({
-  //
+const sequelize = new Sequelize("node-complete", "root", "Welcome@123", {
+  dialect: "mysql",
   host: "localhost",
-  user: "root",
-  database: "node-complete",
-  password: "Welcome@123",
 });
 
-module.exports = pool.promise();
+module.exports = sequelize;
