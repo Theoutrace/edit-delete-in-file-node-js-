1713f4a048da93a664f630c5e13d8af481bb2e51
Theoutrace <prakashkumar.imw@gmail.com>
2023-01-26 01:56:12 +0530

    relation many to many with cart, user, products

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const errorController = require("./controllers/error");
 const sequelize = require("./util/database");
 const Product = require("./models/product");
 const User = require("./models/user");
+const CartItem = require("./models/cart-item");
 
 const app = express();
 
@@ -15,6 +16,7 @@ app.set("views", "views");
 
 const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
+const Cart = require("./models/cart");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
@@ -39,6 +41,11 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
 User.hasMany(Product);
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 // creates tables
 sequelize
   // .sync({ force: true })
@@ -57,7 +64,11 @@ sequelize
     return user;
   })
   .then((user) => {
-    console.log(user);
+    // console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
+    console.log(cart);
     app.listen(3000);
   })
   .catch((error) => {
